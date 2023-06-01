870574bbb047092c0b46a5063a57d91f704d17e6
ABDIEL ANTONIO CORRO <aacorro@outlook.com>
2023-05-31 20:58:18 -0500

    Add project files.

---
+++ Shiny/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Shiny
+{
+    public class Calculator
+    {
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ShinyMSTest/CalculatorMSTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shiny;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShinyMSTest
+{
+    [TestClass]
+    public class CalculatorMSTests
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoInt_GetCorrectAddition()
+        {
+            //Arrange
+            Calculator calc = new();
+
+            //Act
+            int result = calc.AddNumbers(10, 20);
+
+            //Assert
+            Assert.AreEqual(30, result);
+
+        }
+    }
+}
